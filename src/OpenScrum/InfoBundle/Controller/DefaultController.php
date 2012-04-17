e6444617e38d95a3c0cb7fa76622a1f7f4fffb2f
Polonkai Gergely <polesz@w00d5t0ck.info>
2012-04-17 16:46:46 +0000

    Created an initial, viewable version of the site
    Signed-off-by: Gergely POLONKAI (W00d5t0ck) <polesz@w00d5t0ck.info>

--- src/OpenScrum/InfoBundle/Controller/DefaultController.php
+++ src/OpenScrum/InfoBundle/Controller/DefaultController.php
@@ -8,8 +8,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('OpenScrumInfoBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('OpenScrumInfoBundle:' . $this->get('session')->getLocale() . ':index.html.twig', array());
+    }
+
+    public function infopageAction($page)
+    {
+	    return $this->render('OpenScrumInfoBundle:' . $this->get('session')->getLocale() . ':' . $page . '.html.twig', array());
     }
 }
